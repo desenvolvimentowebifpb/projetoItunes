44914dcbb45d2bde19af10fd1d4c77be3e5fdd35
LeonardoCostaVieira <leopanjpmat@gmail.com>
2014-06-13 14:31:00 -0300

    Uso do Padrao DAO Inicio da Geração dos Statment SQL para as classes

---
+++ src/br/com/factory/PreparedStatementFactory.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package br.com.factory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+/**
+ * @author Programador
+ *
+ */
+public class PreparedStatementFactory {
+
+	private PreparedStatement pstm;
+	
+	public PreparedStatement getPreparedStatement(Connection conn, String sql){
+		try { // tentativa de criação do statement
+			this.pstm = conn.prepareStatement(sql);// Criando o preparedStatement
+			//System.out.println("pstm criado!");// Informando ao terminal que o preparedStatement foi criado.
+			return this.pstm; // retornando o stm criado.
+		} catch (SQLException e) {// tratamento de um erro do sql
+			e.toString();
+			return null;// retornando null
+		}catch (NullPointerException e){ //tratamento do erro de conexao nula
+			e.toString();
+			return null;// retornando null
+		}
+	}
+
+}
